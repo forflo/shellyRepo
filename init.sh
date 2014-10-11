21d78aa7fb84fbb5c8bca41ca5fdcd049efbfac5
Florian Mayer <mayerflorian@me.com>
2014-10-11 19:35:24 +0200

    Initial commit

---
+++ init.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# Script that does a complete checkout of all
+# git, svn and mercurial repos listed in
+# shellyRepoConf.sh. The script assumes that the
+# Directory-Structure has already been created 
+# by shellyMeta.
+#
+# Note that this script is intendet to be sourced
+# by setup.sh inside of shellyMeta.
+##
+shellyRepo_init(){
+	local conf="shellyRepoConf.sh"
+	. ${conf} || {
+		clog 1 "[shellyRepo_init()]" Sourcing of $conf failed
+		return 1
+	}
+
+	return 0
+}
+
+##
+# Clones all repos in GIT_REPOLIST, MERCURIAL_REPOLIST
+# and SVN_REPOLIST.
+##
+shellyRepo_dlRepos(){
+	local oldpwd=$PWD
+
+	clog 2 "[shellyRepo_dlRepos()]" Downloading git repos.
+	cd ~/repos/git
+	for i in ${GIT_REPOLIST[*]}; do
+		clog 2 "[shellyRepo_dlRepos()]" Cloging repo $i.
+		git clone ${i} 2> /dev/null || { 
+			clog 1 "[shellyRepo_dlRepos()]" Error while cloning repo $i!
+			clog 1 "[shellyRepo_dlRepos()]" Maybe already existent?
+			return 1
+		}
+	done
+	
+	clog 2 "[shellyRepo_dlRepos()]" Downloading hg repos.
+	cd ~/repos/mercurial
+	for i in ${MERCURIAL_REPOLIST[*]}; do
+		clog 2 "[shellyRepo_dlRepos()]" Cloning repo $i.
+		hg clone ${i} 2> /dev/null || { 
+			clog 1 "[shellyRepo_dlRepos()]" Error while cloning repo $i!
+			clog 1 "[shellyRepo_dlRepos()]" Maybe already existent?
+			return 1
+		}
+	done
+	
+	clog 2 "[shellyRepo_dlRepos()]" Downloading srv repos.
+	cd ~/repos/svn
+	for i in ${SVN_REPOLIST[*]}; do
+		clog 2 "[shellyRepo_dlRepos()]" Checkout of repo $i.
+		svn checkout ${i} || { 
+			clog 1 "[shellyRepo_dlRepos()]" Error while checkout of $i!
+			clog 1 "[shellyRepo_dlRepos()]" Maybe already existent?
+			return 1
+		}
+	done
+
+	cd $OLDPWD_C
+	return 0
+}
+
+init(){
+	shellyRepo_dlRepos || {
+		clog 1 "[init()]" Could not download Repos!
+		exit 1
+	}
+
+	shellyRepo_init || {
+		clog 1 "[init()]" Could not init shellyRepo!
+		exit 1
+	}
+
+	return 0
+}
+
+init
---
+++ shellyRepoConf.sh
@@ -0,0 +1,34 @@
+# The full repolist used by
+# install_repos.sh
+##
+
+GIT_REPOLIST=(
+	"git@github.com:forflo/Maction.git"
+	"git@github.com:forflo/DNSSpackpack"
+	"git@github.com:forflo/admintools"
+	"git@github.com:forflo/shelly"
+	"git@github.com:forflo/wetL"
+	"git@github.com:forflo/libkact"
+	"git@github.com:forflo/dystruct"
+	"git@github.com:forflo/libfce"
+	"git@github.com:forflo/bashm"
+	"git@github.com:forflo/basem"
+	"git@github.com:forflo/snippetbin.git"
+	"git@github.com:forflo/shiny.git"
+	"git@github.com:forflo/libber.git"
+	"git@github.com:forflo/millionaer_server.git"
+	"git@github.com:forflo/erzeuger_verbraucher.git"
+
+	"ssh://florian@klingon.inf.fh-rosenheim.de/srv/git/work"
+)
+
+MERCURIAL_REPOLIST=(
+	"ssh://hg@bitbucket.org/tartaros/fapr0"
+	"ssh://hg@bitbucket.org/forflo/fhbachelor"
+	"ssh://hg@bitbucket.org/forflo/fhmaster"	
+	"ssh://hg@bitbucket.org/tartaros/tartaros-misc"
+)
+
+SVN_REPOLIST=(
+
+)
