c839724a4d57353a3a72217470f86ff6695a0fc2
forflo <forflo@users.noreply.github.com>
2014-10-20 13:11:37 +0200

    Update shellyRepoConf.sh

--- shellyRepoConf.sh
+++ shellyRepoConf.sh
@@ -5,35 +5,16 @@
 
 SHELLYR_pathFile=".paths.conf"
 
-SHELLYR_gitList=(
-	"git@github.com:forflo/Maction.git"
-	"git@github.com:forflo/admintools"
-	"git@github.com:forflo/dmux.git"
-	"git@github.com:forflo/wetL"
-	"git@github.com:forflo/libkact"
-	"git@github.com:forflo/dystruct"
-	"git@github.com:forflo/libfce"
-	"git@github.com:forflo/bashm"
-	"git@github.com:forflo/basem"
-	"git@github.com:forflo/snippetbin.git"
-	"git@github.com:forflo/shiny.git"
-	"git@github.com:forflo/libber.git"
-	"git@github.com:forflo/millionaer_server.git"
-	"git@github.com:forflo/erzeuger_verbraucher.git"
-
-	"ssh://florian@klingon.inf.fh-rosenheim.de/srv/git/work"
-)
-
 SHELLYR_hgList=(
-	"ssh://hg@bitbucket.org/tartaros/fapr0"
-	"ssh://hg@bitbucket.org/tartaros/tartaros-misc"
+	["ssh://hg@bitbucket.org/tartaros/fapr0"]="shelly_fapr0"
+	["ssh://hg@bitbucket.org/tartaros/tartaros-misc"]="shelly_tarmisc"
 )
 
 SHELLYR_svnList=(
 
 )
 
-SHELLYR_ACTIONMAP=(
+SHELLYR_gitList=(
 	["git@github.com:forflo/Maction.git"]="shelly_maction"
 	["git@github.com:forflo/admintools"]="shelly_admintools"
 	["git@github.com:forflo/wetL"]="shelly_wetL"
